ad0e597f07e0cde8bdc2c6f89db9b51ebebd1472
imaa04 <imakhan.04@gmail.com>
2024-04-04 17:29:19 +0100

    add a missing closing bracket from previous merging

--- config/api.js
+++ config/api.js
@@ -26,28 +26,30 @@ export const getNewCoins = (timeFrame = '1 day') => {
             console.error("Error fetching data:", error);
             throw error;
         });
+}
 
 export const getSummary = () => {
-  return fetch(
-    "https://pairs-sniper-api-v1-0-release.onrender.com/api/pairs/summary"
-  )
-    .then((res) => {
-      return res.json();
-    })
-    .then((data) => {
-      return data.pairs;
-    });
+    return fetch(
+        "https://pairs-sniper-api-v1-0-release.onrender.com/api/pairs/summary"
+    )
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            return data.pairs;
+        });
 };
 
 export const getCoinById = (coin_id) => {
-  return fetch(
-    `https://pairs-sniper-api-v1-0-release.onrender.com/api/coins/${coin_id}`
-  )
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      return data.coin;
-    });
+    return fetch(
+        `https://pairs-sniper-api-v1-0-release.onrender.com/api/coins/${coin_id}`
+    )
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            return data.coin;
+        });
 
-};
+}
+
