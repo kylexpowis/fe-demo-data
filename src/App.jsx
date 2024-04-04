b3f078458c25781a215a48e305ea719de185ed8d
Kyle Powis <kylepowis@Kyles-MacBook-Air.local>
2024-04-04 16:34:16 +0000

    complete summary for singlecoinview

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "../src/app.css";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "./components/routes/dashboard/Dashboard";
+import { SingleCoinSummary } from "./components/routes/singleCoin/SingleCoinSummary";
 
 function App() {
   return (
     <Routes>
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/coins/:coin_id" element={<SingleCoinSummary />} />
     </Routes>
   );
 }
--- src/components/routes/dashboard/Dashboard.jsx
+++ src/components/routes/dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Summary } from "../../tables/Summary";
-
+import { SingleCoinSummary } from "../singleCoin/SingleCoinSummary";
 
 function Dashboard() {
   return (
