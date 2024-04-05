7374f89b4d13cc67384e2632a9ac6604822ad8ac
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-04-05 13:15:39 +0100

    remove text white from the first div in landing page

--- src/components/routes/landing/LandingPage.jsx
+++ src/components/routes/landing/LandingPage.jsx
@@ -4,7 +4,7 @@ function LandingPage() {
     return (
         <>
             <Navbar />
-            <div className="flex flex-col justify-center items-center  bg-center text-white" style={{ maxHeight: '100vh', height: '90vh' }}>
+            <div className="flex flex-col justify-center items-center  bg-center" style={{ maxHeight: '100vh', height: '90vh' }}>
                 <div className="text-center px-5 mb-20">
                     <h1 className="text-7xl font-bold mb-4">Your answer to <span className="gradient-text">Financial Analysis.</span></h1>
                     <p className="text-2xl font-light">Invite only access to <span className="gradient-text">analytics</span> with <span className="gradient-text">accuracy</span>.</p>
