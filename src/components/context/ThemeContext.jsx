6532667ef848b566c042319c434ff09ba4aca405
Saabir (ProdiGeeZ) <saabirahmed@live.co.uk>
2024-04-07 15:53:18 +0100

    update themecontext to persist chosen theme

--- src/components/context/ThemeContext.jsx
+++ src/components/context/ThemeContext.jsx
@@ -1,18 +1,27 @@
 import React, { createContext, useState, useEffect } from 'react';
 import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
-import { light, dark } from '../../../theme'; 
+import { light, dark } from '../../../theme';
 import useMediaQuery from '@mui/material/useMediaQuery';
 
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
     const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
-
-    const [theme, setTheme] = useState(prefersDarkMode ? 'dark' : 'light');
+    
+    const [theme, setTheme] = useState(
+        localStorage.getItem('appTheme') || (prefersDarkMode ? 'dark' : 'light')
+    );
 
     useEffect(() => {
-        setTheme(prefersDarkMode ? 'dark' : 'light');
+        const storedTheme = localStorage.getItem('appTheme');
+        if (!storedTheme) {
+            setTheme(prefersDarkMode ? 'dark' : 'light');
+        }
     }, [prefersDarkMode]);
+
+    useEffect(() => {
+        localStorage.setItem('appTheme', theme);
+    }, [theme]);
 
     const toggleTheme = () => {
         setTheme((currentTheme) => (currentTheme === 'light' ? 'dark' : 'light'));
